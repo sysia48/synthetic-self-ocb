8527c8d83417406544ddb8d67b25a1821e0112a6
sysia48 <s.miksztal@gmail.com>
2026-05-03 14:15:42 +0200

    feat: implement blame tree module

--- src/ocb/blame_tree.py
+++ src/ocb/blame_tree.py
@@ -1,25 +1,6 @@
-"""
-Outcome-Centric Blame Tree
-Non-vanishing responsibility assignment.
-"""
-
-class BlameNode:
-    def __init__(self, action, outcome):
-        self.action = action
-        self.outcome = outcome
-        self.children = []
-
-    def add_child(self, node):
-        self.children.append(node)
-
-
 class BlameTree:
     def __init__(self):
-        self.root = None
+        self.log = []
 
-    def assign(self, action, outcome):
-        node = BlameNode(action, outcome)
-        if self.root is None:
-            self.root = node
-        else:
-            self.root.add_child(node)
+    def assign(self, action, reward):
+        self.log.append((action, reward))
