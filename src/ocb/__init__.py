6f9b94bee729bff0cf3a33f4cedef0e6c8c0f042
sysia48 <s.miksztal@gmail.com>
2026-05-03 14:14:37 +0200

    init: add ocb package exports (__init__.py)

--- src/ocb/__init__.py
+++ src/ocb/__init__.py
@@ -1,7 +1,15 @@
-# ocb package
+"""
+OCB — Outcome Consequence Backpropagation
+"""
 
 from .synthetic_self import SyntheticSelf
+from .capacity_forecaster import CapacityForecaster
+from .blame_tree import BlameTree
+from .rollback import RollbackManager
 
 __all__ = [
     "SyntheticSelf",
+    "CapacityForecaster",
+    "BlameTree",
+    "RollbackManager",
 ]
