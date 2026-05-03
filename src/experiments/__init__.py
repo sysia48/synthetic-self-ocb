f4550522d2de65be1958a93dd31578dbcbeeb344
sysia48 <s.miksztal@gmail.com>
2026-05-03 14:07:41 +0200

    init: setup synthetic_self package exports

--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -1,1 +1,11 @@
-# experiments package
+"""
+Experiments module — environments and simulations
+"""
+
+from .bipedal import BipedalEnv
+from .humanoid_v4 import HumanoidEnv
+
+__all__ = [
+    "BipedalEnv",
+    "HumanoidEnv",
+]
